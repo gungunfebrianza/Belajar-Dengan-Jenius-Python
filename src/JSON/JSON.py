870253d7008b9364b21c68bfba2a6f1951ac5738
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-20 01:55:34 +0700

    add comment

--- src/JSON/JSON.py
+++ src/JSON/JSON.py
@@ -6,3 +6,10 @@ jsonvar = json.loads(lover)
 
 print(jsonvar["Name"])  # Maudy
 print(jsonvar["Age"])  # 24
+
+dictionaryprocessor = {"Type": "Intel i7", "Generation":"10th"}  # Python Object Dictionary
+
+jsonstring = json.dumps(dictionaryprocessor)
+
+print(jsonstring)  # {"Type": "Intel i7", "Generation": "10th"}
+print(type(jsonstring))  # <class 'str'>
