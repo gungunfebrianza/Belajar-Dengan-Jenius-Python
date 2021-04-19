6376dc6122d1f0f8dd1b040408860ebf408ef6ff
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-19 16:16:56 +0700

    add python number

---
+++ src/1. Basics/3. Number/1. Python Numbers.py
@@ -0,0 +1,13 @@
+decimalnumber = 3.14
+number = 3
+
+positiveexponent = 123e5
+negativeexponent =123e-5
+
+print(positiveexponent)  # 12300000.0
+print(type(positiveexponent))  #<class 'float'>
+
+print(negativeexponent)  # 0.00123
+print(type(negativeexponent))  #<class 'float'>
+
+
