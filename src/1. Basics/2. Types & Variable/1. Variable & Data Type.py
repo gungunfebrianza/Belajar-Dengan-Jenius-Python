9c58a8c1c19ae7b62513adabbfb20fa1e985b9b2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-19 16:08:40 +0700

    Update Data Type

--- src/1. Basics/2. Types & Variable/1. Variable & Data Type.py
+++ src/1. Basics/2. Types & Variable/1. Variable & Data Type.py
@@ -6,3 +6,21 @@ maudy = 'Maudya Ayunda Faza'  # Single Quote String
 
 logic = True  # Boolean
 
+complex = 5j
+
+listavenger = ["Iron-Man", "Captain America", "Spiderman", "Ant Man"]
+
+tuplecolor = ("Red", "Black", "White")
+
+dictionaryprocessor = {"Type": "Intel i7", "Generation":"10th"}
+
+setanimal = {"Tiger", "Eagle", "Wolf"}
+
+frozensetcurrency = frozenset({"Yen", "Dollar", "Yuan"})
+
+bytesdatatype = b"Data String"
+
+bytearraydatatype = bytearray(100)
+
+memoryviewdatatype = memoryview(bytes(100))
+
