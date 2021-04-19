4456fd283806d259b4bd24a81cb66de455938228
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-19 15:54:45 +0700

    add local

--- src/1. Basics/2. Types & Variable/3. Type Conversion.py
+++ src/1. Basics/2. Types & Variable/3. Type Conversion.py
@@ -4,9 +4,12 @@ pi = 3.14  # Float
 maudya = "Maudy Ayunda"  # Double Quote String
 maudy = 'Maudya Ayunda Faza'  # Single Quote String
 
+ten = "10"
+
 logic = True  # Boolean
 
 print(type(str(pi)))  # 3.14, <class 'str'>
 print(type(str(years)))  # 2018, <class 'str'>
 print(type(str(logic)))  # True, <class 'str'>
-
+print(type(int(ten)))  # 10, <class 'int'>
+print(type(float(years)))  # 2018.0, <class 'float'>
---
+++ src/1. Basics/2. Types & Variable/4. Local & Global Variable.py
@@ -0,0 +1,8 @@
+globalvariable = "belajar dengan jenius python"
+
+def mYfunction():
+    localvariable = "Belajar Dengan Jenius PyQT"
+    print("Dapatkan segera Buku " + localvariable + " Karya Gun Gun Febrianza")
+
+mYfunction()
+
