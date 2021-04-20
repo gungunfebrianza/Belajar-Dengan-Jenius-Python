6f57ccc6af762aa701dfc5867df7cb3ec71227e7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-21 05:10:27 +0700

    add arbitrary keyword

---
+++ src/Function/4. Arbitrary Keyword Arguments.py
@@ -0,0 +1,13 @@
+def displayName(**args):
+    print(args["Name"])
+    print(args["Age"])
+
+
+displayName(Name="Maudy", Age=25)
+
+lover = {
+    "Name":"Maudy Ayunda Faza",
+    "Age":25
+}
+
+#  displayName(lover) #Does Not Work
