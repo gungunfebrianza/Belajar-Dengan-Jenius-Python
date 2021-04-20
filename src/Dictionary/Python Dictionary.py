a7a22ab019c18f728f76cb216076a9750e6d8c94
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-21 04:32:39 +0700

    add Dictionary

--- src/Dictionary/Python Dictionary.py
+++ src/Dictionary/Python Dictionary.py
@@ -1,7 +1,22 @@
+
+# CREATION
 dictionarymaudy = {
     "Name":"Maudy",
     "Age":24,
-    "Profession":"Singer"
+    "Profession":"Singer",
+    "Songs": ["Perahu Kertas", "By My Side", "Untuk Apa"]
 }
 
-print(dictionarymaudy)
+print(dictionarymaudy)  # {'Name': 'Maudy', 'Age': 24, 'Profession': 'Singer'}
+
+
+# ACCESS ITEM
+print(dictionarymaudy.get("Name"))  # Maudy
+print(dictionarymaudy["Age"])  # 24
+print(dictionarymaudy.get("SONGS"))  # None
+print(dictionarymaudy.keys())  # dict_keys(['Name', 'Age', 'Profession', 'Songs'])
+print(dictionarymaudy.values())  # dict_values(['Maudy', 24, 'Singer', ['Perahu Kertas', 'By My Side', 'Untuk Apa']])
+
+
+print(len(dictionarymaudy))  # 3
+
