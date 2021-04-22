0f2b8cad11aab713b8bae08f88ad9eed067b5b84
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-22 21:25:02 +0700

    add list

--- src/List/1. List.py
+++ src/List/1. List.py
@@ -1,2 +1,7 @@
 animal = ['Snake', 'Wolf', 'Monkey', 'Tiger', 'Tiger']
 print(animal.count('Tiger'))  # 2
+print(animal.index('Snake'))  # 0
+print(animal.index('Wolf'))  # 1
+print(animal)
+
+print(type(animal))  # <class 'list'>
