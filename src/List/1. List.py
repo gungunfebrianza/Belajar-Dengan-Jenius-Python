61a8eca83d4dc8e5fabaa17f3c25ea094b4ff0a8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-23 01:03:36 +0700

    access list

--- src/List/1. List.py
+++ src/List/1. List.py
@@ -1,7 +1,24 @@
+# CREATE LIST
 animal = ['Snake', 'Wolf', 'Monkey', 'Tiger', 'Tiger']
+
+# ACCESS ITEMS
+print(animal[0])
+print(animal[-1])  # Negative Indexing
+print(type(animal[-1]))  # <class 'str'>
+print(animal[1:3])  # Range of Indexes
+print(type(animal[1:3]))  # <class 'list'>
+
 print(animal.count('Tiger'))  # 2
 print(animal.index('Snake'))  # 0
 print(animal.index('Wolf'))  # 1
+print(len(animal))  # 5
+print(type(animal))  # <class 'list'>
 print(animal)
 
-print(type(animal))  # <class 'list'>
+# LIST DATA TYPES
+liststring = ["Indonesia", "Japan", "South Korea", "Thailand"]
+listinteger = [44, 55, 66, 77, 88, 99]
+listboolean = [True, False, True, False]
+listheterogen = ["Avenger", 99, False]
+
+
