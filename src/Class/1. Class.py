9735ead77bd54e84e309a979503eb774c7870c78
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-23 00:32:20 +0700

    add class

---
+++ src/Class/1. Class.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, firstname, lastname, age, eyecolor):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.eyecolor = eyecolor
+
