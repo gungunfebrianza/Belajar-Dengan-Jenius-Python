f1c52e14068b05b60fda16f9efc1b26c6c9d8cf3
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-24 23:24:09 +0700

    add example

--- src/Tupple/1. Tupple.py
+++ src/Tupple/1. Tupple.py
@@ -11,6 +11,19 @@ tupplestringheterogen = ("Avenger", 99, False)
 
 # ACCESS TUPPLES
 print(animal[1])  # Anoa
+print(animal[-1])  # Eagle
+print(animal[0:2])  # ('Boar', 'Anoa')
+print(type(animal[0:2]))  # <class 'tuple'>
+print(animal[0:])  # ('Boar', 'Anoa', 'Eagle', 'Eagle')
+print(animal[1:])  # ('Anoa', 'Eagle', 'Eagle')
+print(animal[2:])  # ('Eagle', 'Eagle')
+print(animal[3:])  # ('Eagle',)
+print(animal[4:])  # ()
+print(animal[52:])  # ()
+print(animal[:0])  # ()
+print(animal[:1])  # ('Boar',)
+print(animal[:2])  # ('Boar', 'Anoa')
+print(animal[:3])  # ('Boar', 'Anoa', 'Eagle')
 
 # COUNT TUPPLE
 print(len(animal))  # 4
