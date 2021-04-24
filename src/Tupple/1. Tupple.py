75857ee25883504911ddeceb469912cc9b729ab6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-24 23:19:38 +0700

    access tupples

--- src/Tupple/1. Tupple.py
+++ src/Tupple/1. Tupple.py
@@ -1,6 +1,16 @@
 # CREATE TUPPLE
 animal = ("Boar", "Anoa", "Eagle", "Eagle")
 print(animal)
+print(type(animal))  # <class 'tuple'>
+
+# LIST DATA TYPES
+tupplestring = ("Indonesia", "Japan", "South Korea", "Thailand")
+tupplestringinteger = (44, 55, 66, 77, 88, 99)
+tupplestringboolean = (True, False, True, False)
+tupplestringheterogen = ("Avenger", 99, False)
+
+# ACCESS TUPPLES
+print(animal[1])  # Anoa
 
 # COUNT TUPPLE
 print(len(animal))  # 4
