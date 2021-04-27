76d234410619e16ee0ed1f0f296850ea08591459
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-28 00:29:23 +0700

    add sys modules

--- src/Modules/sys.py
+++ src/Modules/sys.py
@@ -8,17 +8,3 @@ for i in range(len(sys.argv)):
     else:
         print(f"{i:1d}. Argument: {sys.argv[i]}")
 
-print("Coming through stdout")
-
-# stdout is saved
-save_stdout = sys.stdout
-
-fh = open("test.txt","w")
-
-sys.stdout = fh
-print("This line goes to test.txt")
-
-# return to normal:
-sys.stdout = save_stdout
-
-fh.close()
---
+++ src/Modules/sys3.py
@@ -0,0 +1,7 @@
+import sys
+
+
+print(sys.platform)  # Name of the platform on which Python is running
+print(sys.version)  # Version number of the Python interpreter
+print(sys.maxsize)  # The largest positive integer supported by the platform's
+print(sys.executable)
