ddedbf3909a4dc4a9ad56f07549744f48eb413c0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-28 00:24:04 +0700

    add sys modules

--- src/Modules/sys2.py
+++ src/Modules/sys2.py
@@ -1,13 +1,5 @@
 import sys
 
-print(sys.argv)
-
-for i in range(len(sys.argv)):
-    if i == 0:
-        print("Function name: ", sys.argv[0])
-    else:
-        print(f"{i:1d}. Argument: {sys.argv[i]}")
-
 print("Coming through stdout")
 
 # stdout is saved
