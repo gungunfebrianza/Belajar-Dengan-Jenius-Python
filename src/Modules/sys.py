c5e7fa9b0ef158b2031e5c6ac75a5e1404550a2f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-28 00:17:23 +0700

    add sys modules

---
+++ src/Modules/sys.py
@@ -0,0 +1,12 @@
+import sys
+
+print(sys.argv)
+
+for i in range(len(sys.argv)):
+    if i == 0:
+        print("Function name: ", sys.argv[0])
+    else:
+        print(f"{i:1d}. Argument: {sys.argv[i]}")
+
+
+a
