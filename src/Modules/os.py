5fb12d85455435dfe6c07101a8859c0e90463857
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-04-28 03:45:39 +0700

    add mosule os

---
+++ src/Modules/os.py
@@ -0,0 +1,8 @@
+import os
+
+print(os.name)
+print(os.getcwd())
+os.chdir(r"F:\The Alexandria\Belajar Dengan Jenius Python\Github\Belajar-Dengan-Jenius-Python\src")
+
+print(os.listdir())
+print(type(os.listdir()))  # <class 'list'>
