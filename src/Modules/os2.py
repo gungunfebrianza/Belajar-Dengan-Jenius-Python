1af6ba06330eb5f5ac222012351d0098813f99fe
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-05-02 03:01:15 +0700

    addPyQT

--- src/Modules/os2.py
+++ src/Modules/os2.py
@@ -3,3 +3,10 @@ import os
 cmd = "notepad"
 os.system(cmd)
 
+# MAKE DIRECTORY
+parent_directory = "D:\\"
+directory = "pintercoding"
+path = os.path.join(parent_directory,directory)
+
+os.mkdir(path)
+print("Directory '% s' created" % directory)
