085a621ec8d28ae9a03bfe2baff290c72237a26c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-05-03 16:47:59 +0700

    add QMessageBox

--- src/PyQT/QWidget - QMessageBox.py
+++ src/PyQT/QWidget - QMessageBox.py
@@ -1,36 +1,40 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtWidgets
 
 
 class Window(QWidget):
     def __init__(self):
         QWidget.__init__(self)
+        # layout = QGridLayout()
+        # self.setLayout(layout)
         # Add MessageBox / QMessageBox
         self.mbox1 = QMessageBox()
 
+        # Add Button / QPushButton
+        self.btn1 = QPushButton()
+        self.btn1 = QtWidgets.QPushButton(self)
+        # Set Button Properties
+        self.btn1.setText("Click Here")
+        self.btn1.setGeometry(QtCore.QRect(170, 50, 75, 23))
+        self.btn1.clicked.connect(self.display_message)
+        # layout.addWidget(self.btn1)
 
-def display_message(self):
-    # Set MessageBox Properties
-    self.mbox1.setWindowTitle("Message Box")
-    self.mbox1.setText("Hello World, Maudy !")
-    self.mbox1.setDetailedText("Place for more details information.")
-    self.mbox1.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-    return_value = self.mbox1.exec()
-    if return_value == QMessageBox.Ok:
-        print('OK clicked')
+    def display_message(self):
+        # Set MessageBox Properties
+        self.mbox1.setWindowTitle("Message Box")
+        self.mbox1.setText("Hello World, Maudy !")
+        self.mbox1.setDetailedText("Place for more details information.")
+        self.mbox1.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        return_value = self.mbox1.exec()
+        if return_value == QMessageBox.Ok:
+            print('OK clicked')
 
 
 app = QApplication(sys.argv)  # QApplication([])
-window = QWidget()
+window = Window()
 window.setWindowTitle("Main Window - QMessageBox")  # Window Title
 window.resize(400, 400)  # Resize Window
-
-# Add Button / QPushButton
-btn1 = QPushButton(window)
-# Set Button Properties
-btn1.setText("Click Here")
-btn1.move(100, 40)
-btn1.clicked.connect(display_message)
 
 window.show()
 # Start the event loop.
