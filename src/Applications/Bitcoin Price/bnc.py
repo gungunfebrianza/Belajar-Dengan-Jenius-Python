098e5e41be61d90b9d4c824831d70c6fd06dc249
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-05-13 04:09:31 +0700

    add okex api

--- src/Applications/Bitcoin Price/bnc.py
+++ src/Applications/Bitcoin Price/bnc.py
@@ -1,9 +1,25 @@
 import json
 import datetime
+import os
+import okex.Account_api as Account
+
 
 def get_timestamp():
     now = datetime.datetime.now()
     t = now.isoformat("T", "milliseconds")
     return t + "Z"
 
+
 time = get_timestamp()
+
+if __name__ == '__main__':
+
+    api_key = "7b7d4471-dc15-4891-b017-e5ffdd2b728"
+    secret_key = "4C49046150E81FAFDB93560F02F5334"
+    passphrase = "lamborGhini"
+    flag = '1'  # 模拟盘 demo trading
+    # flag = '0'  # 实盘 real trading
+
+accountAPI = Account.AccountAPI(api_key, secret_key, passphrase, False, flag)
+result = accountAPI.get_account('UNI')
+print(result)
