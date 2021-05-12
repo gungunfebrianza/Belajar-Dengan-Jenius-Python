04ae13d6c745443e42ae4217646741448c1586c9
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-05-12 20:44:55 +0700

    add timestamp

--- src/Applications/Bitcoin Price/bnc.py
+++ src/Applications/Bitcoin Price/bnc.py
@@ -1,8 +1,9 @@
-from binance.client import Client
+import json
+import datetime
 
-api_key = "KWtCRlBZvgKTYF9GGj29WDGB2Pyl4IpLKySuIy89XYwlD8NgFmuYJgzeX5p4w3ef"
-api_secret = "RtqWaTVphzdYgLsyERUKN7dZsoOm4ajAfqhu6kI4BdeIwcpBLoGIpO8F98K87olY"
+def get_timestamp():
+    now = datetime.datetime.now()
+    t = now.isoformat("T", "milliseconds")
+    return t + "Z"
 
-client = Client(api_key, api_secret)
-prices = client.get_all_tickers()
-print(prices)
+time = get_timestamp()
