3067456b7cd23cd634135ae7ed7bf543645c77c0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-05-14 06:35:56 +0700

    add select command python

---
+++ src/MySQL/select-command.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+my_database = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="test",
+)
+
+my_cursor = my_database.cursor()
+
+# PULL DATA FROM THE TABLE
+my_cursor.execute("SELECT * FROM users")
+result = my_cursor.fetchall()
+print("NAME\tEMAIL\t\t\tAGE\tID")
+print("----\t-----\t\t\t---\t---")
+for row in result:
+    print(row[0] + "\t%s" % row[1] + "\t\t%s" % row[2] + "\t%s" % row[3])
