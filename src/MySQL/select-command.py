2e39f6b10cf5391c5ec32359ff0444e3dda28695
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-05-14 06:37:09 +0700

    add where clause command python

--- src/MySQL/select-command.py
+++ src/MySQL/select-command.py
@@ -16,3 +16,21 @@ print("NAME\tEMAIL\t\t\tAGE\tID")
 print("----\t-----\t\t\t---\t---")
 for row in result:
     print(row[0] + "\t%s" % row[1] + "\t\t%s" % row[2] + "\t%s" % row[3])
+
+# WHERE CLAUSE
+my_cursor.execute("SELECT * FROM users WHERE name =  'John'")
+result = my_cursor.fetchall()
+for row in result:
+    print(row)
+
+# WHERE and LIKE WILDCARDS
+my_cursor.execute("SELECT * FROM users WHERE name LIKE '%i%'")
+result = my_cursor.fetchall()
+for row in result:
+    print(row)
+
+# AND / OR Clause
+my_cursor.execute("SELECT * FROM users WHERE name LIKE '%i%' AND age = 29 AND user_id = 5")
+result = my_cursor.fetchall()
+for row in result:
+    print(row)
