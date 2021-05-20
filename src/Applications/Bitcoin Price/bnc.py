6606142f1fc92bdbfa56868613dc703902f238a7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-05-21 06:27:41 +0700

    add MarketAPI

--- src/Applications/Bitcoin Price/bnc.py
+++ src/Applications/Bitcoin Price/bnc.py
@@ -14,9 +14,9 @@ time = get_timestamp()
 
 if __name__ == '__main__':
 
-    api_key = "7b7d4471-dc15-4891-b017-e5ffdd2b728"
-    secret_key = "4C49046150E81FAFDB93560F02F5334"
-    passphrase = "lamborGhini"
+    api_key = "7b7d4471-dc15-4891-b017-e5ffdd2b7284"
+    secret_key = "4C49046150E81FAFDB93560F02F5334F"
+    passphrase = "lamborGhini2019"
     flag = '1'  # 模拟盘 demo trading
     # flag = '0'  # 实盘 real trading
 
